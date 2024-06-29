ed3ff7731739a3caa695acc14afb3c7befd4930c
Herman Proshunin <g.proshunin15@gmail.com>
2024-06-29 21:02:08 +0500

    Add cell open and flag set

--- Tile.gd
+++ Tile.gd
@@ -1,9 +1,36 @@
 extends Node2D
 
+var is_covered = true
+var flagged    = false
+var is_mine    = false
+
+
+#func _ready():
+
+func set_bomb():
+	is_mine = true
+	$Bomb.show()
+	
+
+func uncover():
+	if flagged == false:
+		$Cover.hide()
+		is_covered = false
+
+
+func toggle_flag():
+	if is_covered:
+		if flagged:
+			$Flag.hide()
+			flagged = false
+		else:
+			$Flag.show()
+			flagged = true
+
+
 func _on_control_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.is_action_pressed("left_click"):
-			print("Left mouse button pressed")
+			uncover()
 		if event.is_action_pressed("right_click"):
-			print("Right mouse button pressed")
-			
+			toggle_flag()
